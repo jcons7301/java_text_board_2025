9007cc7c96b93c5d327220d4bb5a9dde68bd4d40
jcons7301 <jcons880@gmail.com>
2025-03-30 09:46:17 +0900

    명령어 입력 받기

--- src/main/java/com/sbs/exam/board/Main.java
+++ src/main/java/com/sbs/exam/board/Main.java
@@ -1,13 +1,24 @@
 package com.sbs.exam.board;
 
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
   public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
     System.out.println("=== 텍스트 게시판 v 0.1==");
     System.out.println("프로그램 시작");
+
+    System.out.printf("명령) ");
+    String cmd = sc.nextLine();
+
+    System.out.printf("입력받은 명령어 : %s\n", cmd);
+
+
     System.out.println("프로그램 끝");
     System.out.println("=== 게시판을 종료합나디 ===");
     }
