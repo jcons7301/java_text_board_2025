a9098603a447eefbe979ff3b246ebe40594c008b
jcons7301 <jcons880@gmail.com>
2025-03-30 09:56:08 +0900

    명령어 연속해서 입력받기

--- src/main/java/com/sbs/exam/board/Main.java
+++ src/main/java/com/sbs/exam/board/Main.java
@@ -13,14 +13,18 @@ public class Main {
     System.out.println("=== 텍스트 게시판 v 0.1==");
     System.out.println("프로그램 시작");
 
-    System.out.printf("명령) ");
-    String cmd = sc.nextLine();
 
-    System.out.printf("입력받은 명령어 : %s\n", cmd);
+    while (true) {
+      System.out.printf("명령) ");
+      String cmd = sc.nextLine();
 
+      if (cmd.equals("exit")) {
+        System.out.println("=== 게시판을 종료합나디 ===");
+        break;
+      }
+      System.out.printf("입력받은 명령어 : %s\n", cmd);
 
-    System.out.println("프로그램 끝");
-    System.out.println("=== 게시판을 종료합나디 ===");
+      sc.close();
     }
-
+  }
 }
