3507ccbe10908b5383be010e767e9b2f3bbfc7bf
jcons7301 <jcons880@gmail.com>
2025-03-30 09:37:50 +0900

    프로그램 시작 메시지

--- src/main/java/com/sbs/exam/board/Main.java
+++ src/main/java/com/sbs/exam/board/Main.java
@@ -6,7 +6,10 @@ public class Main {
   public static void main(String[] args) {
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
-    System.out.println("hello world");
+    System.out.println("=== 텍스트 게시판 v 0.1==");
+    System.out.println("프로그램 시작");
+    System.out.println("프로그램 끝");
+    System.out.println("=== 게시판을 종료합나디 ===");
     }
 
 }
